39d648ef4eff0cd3d7032b27fd75484abee68cfc
Axel Niklasson <axel.niklasson@live.com>
2017-07-17 16:16:08 +0200

    CRUD work on divers

--- app/models/diver.js
+++ app/models/diver.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 
 var DiverSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true }
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    facebookID: { type: Number, required: true },
+    organisation: { type: String, required: true },
+    certification: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Diver', DiverSchema);
--- app/routes/divers.js
+++ app/routes/divers.js
@@ -3,12 +3,33 @@ var router = express.Router();
 var Diver = require('../models/diver');
 
 router.get('/', function(req, res) {
-
     Diver.find({}).sort({ name: 1 }).exec(function(err, divers) {
         if (err) {
             res.status(500).json({ 'Error': 'Could not get divers. Stack trace: ' + err });
         } else {
             res.json(divers);
+        }
+    });
+});
+
+router.get('/exists', function(req, res) {
+    var facebookID = req.query.facebookID;
+
+    Diver.findOne({ facebookID: facebookID }).exec(function(err, diver) {
+        if (err) {
+            res.status(500).json({ 'Error': 'Could not find diver. Stack trace: ' + err });
+        } else {
+            res.json(diver)
+        }
+    })
+});
+
+router.post('/', function(req, res) {
+    Diver.create(req.body, function(err, diver) {
+        if (err) {
+            res.status(500).json({ 'Error': 'Could not create diver. Stack trace: ' + err });
+        } else {
+            res.json(diver);
         }
     });
 });
