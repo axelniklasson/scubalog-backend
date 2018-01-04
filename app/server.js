96983add501dc7669df858c0598575430d6fe629
Gogs <gogs@fake.local>
2018-01-04 20:27:51 +0100

    Fixed env files and start scripts

--- app/server.js
+++ app/server.js
@@ -6,6 +6,13 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
+// Environment settings
+if (process.env.NODE_ENV == 'DEV') {
+    require('./dev.env.js');
+} else {
+    require('./prod.env.js');
+}
+
 // Init express app
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -13,11 +20,6 @@ var PORT = process.env.PORT || 8080;
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Environment settings
-if (process.env.NODE_ENV == 'DEV') {
-    require('./env.js');
-}
 
 // Connect to DB
 var dbHost = 'mongodb://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASS + '@' + process.env.MONGO_HOST + ':' + process.env.MONGO_PORT + '/' + process.env.MONGO_DATABASE;
